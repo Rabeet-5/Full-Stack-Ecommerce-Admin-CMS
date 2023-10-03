916dd3eaad663cf1620e90875f9c3c0077a19b94
Rabeet-5 <rabeetsati@gmail.com>
2023-10-03 14:42:50 +0500

    Setted Project

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+    <div>
+      hello Admin Dashboard 
+      <Button className="ml-5 p-4">Click me !</Button>
+    </div>
+    )
+  }
